7d305d55c5201f69a8d7304d4be47d030e838d83
test1 <test_u01@163.com>
2016-06-07 18:28:32 +0800

    修改

--- api.php
+++ api.php
@@ -20,12 +20,12 @@ class wechatCallbackapiTest
         	exit;
         }
     }
-
+    //回复信息
     public function responseMsg()
     {
 		//get post data, May be due to the different environments
 		$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
-
+        echo $postStr ;die;
       	//extract post data
 		if (!empty($postStr)){
                 /* libxml_disable_entity_loader is to prevent XML eXternal Entity Injection,
